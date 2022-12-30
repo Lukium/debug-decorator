50fe552887d417c665ab08e765f699754e930fea
Lukium <99280463+Lukium@users.noreply.github.com>
2022-12-30 17:41:39 -0500

    Update decorators.py

--- decorators.py
+++ decorators.py
@@ -1,32 +1,54 @@
 from os import get_terminal_size
 
-DEBUG = True
+DEBUG_ON = True
 DEBUG_WIDTH = get_terminal_size().columns
 KEY = {}
 
-def debug(*decoratorargs, **decoratorkwargs):
+def DEBUG(**decoratorkwargs):
     def decorator(function):
-        global DEBUG, DEBUG_WIDTH
+        global DEBUG_ON, DEBUG_WIDTH
         async def wrapper(*args, **kwargs):
             global KEY
             KEY = decoratorkwargs
-            if DEBUG:
+            if DEBUG_ON:
                 print(f'\n╔{str("{:═^{}}").format(f" {function.__name__} Started ", DEBUG_WIDTH -2)}╗') # Creates a centered string ('^') padded with ═ (':═') of custom length ('inner {}')
                 if args:
-                    print(f' Positional Arguments:')
-                    for x in args:
-                        print(f' {str(x)}')
+                    print(f'╠{str("{:═^{}}").format(f" Positional Arguments: ", DEBUG_WIDTH -2)}╣')
+                    for arg in args:
+                        arg_string = f'{str(arg)}'
+                        if len(arg_string) > DEBUG_WIDTH - 4:
+                            chunks = [arg_string[i:i+(DEBUG_WIDTH - 4)] 
+                                for i in range(0, len(arg_string), (DEBUG_WIDTH - 4))]
+                            for chunk in chunks:
+                                print(f'║ {str("{: <{}}").format(chunk, DEBUG_WIDTH - 4)} ║')
+                        else:
+                            print(f'║ {str("{: <{}}").format(arg_string, DEBUG_WIDTH - 4)} ║')
                 if kwargs:
-                    print(f' Keyword Arguments:')
+                    print(f'╠{str("{:═^{}}").format(f" Keyword Arguments: ", DEBUG_WIDTH - 2)}╣')
                     for arg, value in kwargs.items():
-                        print(f' {str(arg)}: {str(value)}')
-                print(f'╠{str("{:═^{}}").format(f"", DEBUG_WIDTH -2)}╣')
+                        arg_string = f'{str(arg)}: {str(value)}'
+                        if len(arg_string) > DEBUG_WIDTH - 4:
+                            chunks = [arg_string[i:i+(DEBUG_WIDTH - 4)] 
+                                for i in range(0, len(arg_string), (DEBUG_WIDTH - 4))]
+                            for chunk in chunks:
+                                print(f'║ {str("{: <{}}").format(chunk, DEBUG_WIDTH - 4)} ║')
+                        else:
+                            print(f'║ {str("{: <{}}").format(arg_string, DEBUG_WIDTH - 4)} ║')
+                print(f'╠{str("{:═^{}}").format(f"", DEBUG_WIDTH - 2)}╣')
             result = await function(*args, **kwargs)
-            if DEBUG:
+            if DEBUG_ON:
                 for arg in KEY:
-                    print(f'{str(arg)}: {str(KEY[arg])}')
+                    arg_string = f'{str(arg)}: {str(KEY[arg])}'
+                    if len(arg_string) > DEBUG_WIDTH - 4:
+                        chunks = [arg_string[i:i+(DEBUG_WIDTH - 4)] 
+                            for i in range(0, len(arg_string), (DEBUG_WIDTH - 4))]
+                        for chunk in chunks:
+                            print(f'║ {str("{: <{}}").format(chunk, DEBUG_WIDTH - 4)} ║')
+                    else:
+                        print(f'║ {str("{: <{}}").format(arg_string, DEBUG_WIDTH - 4)} ║')
                 print(f'╠{str("{:═^{}}").format(f"", DEBUG_WIDTH -2)}╣')
                 print(f'╚{str("{:═^{}}").format(f" {function.__name__} Ended ", DEBUG_WIDTH -2)}╝\n')
+                KEY = {}
             return result
         return wrapper
     return decorator
